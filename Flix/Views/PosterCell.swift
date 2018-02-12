becb352f69daab458a98ea569c1197a0c612e4d9
nikola <nikolachen1203@gmail.com>
2018-02-11 16:51:00 -0800

    added collection view

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Jinhua on 2/11/18.
+//  Copyright © 2018 Jinhua Chen. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
